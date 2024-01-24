4f7f175fda235b4435d4165657479ac74440ac65
vasily <vasilymishanin@gmail.com>
2024-01-24 12:34:10 +0600

    file system functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,41 @@
+import fs from 'fs';
+import { mkdir, readdir, copyFile } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  if (!fs.existsSync('./files')) {
+    throw new Error('FS operation failed - /files not exists ');
+  } else if (fs.existsSync('./files_copy')) {
+    throw new Error('FS operation failed - /files_copy was already created');
+  } else {
+    copyFiles('./files', './files_copy');
+  }
 };
 
 await copy();
+
+async function copyFiles(sourceDir, destinationDir) {
+  if (!fs.existsSync(sourceDir)) {
+    throw new Error('Source directory does not exist: ' + sourceDir);
+  }
+
+  if (!fs.existsSync(destinationDir)) {
+    mkdir(destinationDir);
+  }
+
+  const files = await readdir(sourceDir, (err, files) => {
+    if (err) {
+      throw new Error(err);
+    }
+    return files;
+  });
+
+  try {
+    for (const file of files) {
+      await copyFile(`${sourceDir}/${file}`, `${destinationDir}/${file}`);
+      //console.log('copy - ', file); // check the order
+    }
+    console.log('Files successfully copied');
+  } catch (err) {
+    console.log('ERROR while copying files: ', err);
+  }
+}
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  if (!fs.existsSync('./files/fresh.txt')) {
+    fs.writeFile('./files/fresh.txt', 'I am fresh and young', () => {
+      console.log('New file created');
+    });
+  } else {
+    throw Error('FS operation failed - already exists');
+  }
 };
 
 await create();
