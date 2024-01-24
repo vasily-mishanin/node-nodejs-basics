64327eff5d92e6b820d17f0fd3408f04ec1db10b
vasily <vasilymishanin@gmail.com>
2024-01-24 13:15:32 +0600

    cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const printItems = [];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith('--')) {
+      printItems.push(`${args[i]} is ${args[i + 1]}`);
+      i++;
+    } else {
+      printItems.push(args[i]);
+    }
+  }
+
+  console.log(printItems.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env).filter((entry) =>
+    entry[0].startsWith('RSS_')
+  );
+  console.log(rssVars.map(([key, value]) => `${key}=${value};`).join(' '));
 };
 
 parseEnv();
